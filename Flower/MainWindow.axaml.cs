03cae097130222ce1843061463ce36ba706f914a
Yynor <awesome.nn44@yandex.ru>
2025-05-10 15:00:58 +0300

    Сделана авторизация, вроде бы полностью

--- Flower/MainWindow.axaml.cs
+++ Flower/MainWindow.axaml.cs
@@ -4,7 +4,6 @@ using Avalonia.Interactivity;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
 
-
 namespace Flower;
 
 public partial class MainWindow : Window
@@ -14,40 +13,57 @@ public partial class MainWindow : Window
         InitializeComponent();
     }
 
-
-  
-
-    private async void Button_OnClick(object sender, RoutedEventArgs e)
+    private void Button_OnClick(object sender, RoutedEventArgs e)
     {
-        
-        await Task.Run(() => 
+        try
         {
             var password = PasswordTextBox.Text;
             string stringconnection = "Server=localhost;Database=FlowerDB;User Id=root;Password=;";
-            string query = "SELECT Login,Password,Role  FROM User WHERE `Login` = @username AND `Password` = @password And `Role` = @role";
+            string query = "SELECT Login, Password, Role FROM User WHERE `Login` = @username AND `Password` = @password";
             string passwordDB = PasswordTextBox.Text;
             string loginDB = LoginTextBox.Text;
-                    
+
             MySqlConnection con = new MySqlConnection(stringconnection);
-             con.OpenAsync();
-        
+            con.Open();
+
             MySqlCommand cmd = new MySqlCommand(query, con);
             cmd.Parameters.AddWithValue("@username", loginDB);
             cmd.Parameters.AddWithValue("@password", passwordDB);
-            
+
             MySqlDataReader reader = cmd.ExecuteReader();
             if (reader.HasRows)
             {
+                reader.Read();
+                int role = reader.GetInt32("Role"); 
+
                 Hide();
-             //   MainEmpty empty = new MainEmpty();
-                //empty.Show();
+                
+                if (role == 0)
+                {
+                    AdminWindow adminWindow = new AdminWindow();
+                    adminWindow.Show();
+                }
+                else if (role == 1)
+                {
+                    EmployeeWindow emploeeWindow = new EmployeeWindow();
+                    emploeeWindow.Show();
+                }
+                else if (role == 2)
+                {
+                    Delivery deliveryWindow = new Delivery();
+                    deliveryWindow.Show();
+                }
+                
                 this.Close();
             }
             else
             {
-            //    OshibkaLabel.Content = "Неверно введена почта или пароль";
+                OshibkaLabel.Content = "Неверно введена почта или пароль";
             }
-        });
-        
+        }
+        catch (Exception ex)
+        {
+            OshibkaLabel.Content = ex.Message;
+        }
     }
 }
