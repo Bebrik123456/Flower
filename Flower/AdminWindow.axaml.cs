a2399bc0fdd3fd8c962ae5385e18487f9a912912
Михатл <mikhailcher2006@gmail.com>
2025-05-16 23:24:59 +0300

    В окне администратора добавлен вывод всех заказов(нужно доработать чтобы при нажатии менялся статус оплаты)

--- Flower/AdminWindow.axaml.cs
+++ Flower/AdminWindow.axaml.cs
@@ -2,6 +2,8 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
 
 namespace Flower;
 
@@ -10,12 +12,13 @@ public partial class AdminWindow : Window
     public AdminWindow()
     {
         InitializeComponent();
+        LoadOrders();
     }
 
 
     private void CreateButton_OnClick(object? sender, RoutedEventArgs e)
     {
-        Create create = new Create();   
+        Create create = new Create();
         Hide();
         create.Show();
         this.Close();
@@ -29,5 +32,69 @@ public partial class AdminWindow : Window
         this.Close();
     }
 
+    private void LoadOrders()
+    {
+        // 1. Очищаем список
+        OrdersListBox.Items.Clear();
 
+        // 2. Получаем данные из БД
+        var orders = GetOrdersFromDatabase();
+
+        // 3. Заполняем ListBox вручную
+        foreach (var order in orders)
+        {
+            // Формируем строку для отображения
+            string orderInfo = $"{order.Id} | {order.Name} | {order.Adress} | {order.PaymentStatus} | {order.Accepted} | {order.ReadyForDelivery} ";
+            OrdersListBox.Items.Add(orderInfo); // Просто добавляем текст
+        }
+    }
+    private List<Order> GetOrdersFromDatabase()
+    {
+        var orders = new List<Order>();
+
+        var conn = new MySqlConnection("Server=localhost;Database=FlowerDB;User Id=root;Password=;");
+        {
+            conn.Open();
+            var cmd = new MySqlCommand("SELECT * FROM FlowerDB.Task", conn);
+            var reader = cmd.ExecuteReader();
+            {
+                while (reader.Read())
+                {
+                    orders.Add(new Order
+                    {
+                        Id = reader.GetInt32("ID"),
+                        Name = reader.GetString("Name"),
+                        Adress = reader.GetString("Adress"),
+                        PaymentStatus = reader.GetString("PaymentStatus"),
+                        Accepted = reader.GetString("Accepted"),
+                        ReadyForDelivery = reader.GetString("ReadyForDelivery")
+                    });
+                }
+            }
+        }
+
+        return orders;
+    }
+
+
+
+
+
+    
+
+
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Adress { get; set; }
+
+        public string PaymentStatus { get; set; }
+
+        public string Accepted { get; set; }
+
+        public string ReadyForDelivery { get; set; }
+
+    }
 }
